f2110bf039addd0eca8ad2d0fee5104700bcdfc3
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-07 21:43:17 +0530

    route to home.html

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView
 
 urlpatterns = [
-    path("", homePageView, name="home"),
+    path("", HomePageView.as_view(), name="home"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 # Create your views here.
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse("Hello, World!")
+class HomePageView(TemplateView):
+    template_name = "home.html"
