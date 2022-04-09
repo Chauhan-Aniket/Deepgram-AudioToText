545006b3161fea2577bd3f725f63f162ecf44eb8
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-09 12:15:00 +0530

    set url for upload-file

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, Audio_store
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-]
+    path("audio/", Audio_store)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
