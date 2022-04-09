b87da0a260adf6e7fdd98b633fe3761572d50dcd
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-09 12:12:58 +0530

    created function for upload-file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,20 @@
 # Create your views here.
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import AudioForm
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+def Audio_store(request):
+    if request.method == 'POST':
+        form = AudioForm(request.POST, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return render(request, 'home.html')
+    else:
+        form = AudioForm()
+    return render(request, 'audio.html', {'form': form})
