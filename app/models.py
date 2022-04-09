a85e2d5bcaeeef411fdb7cafa33d83f0b4dd3953
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-09 12:39:48 +0530

    delete previous file on each-upload

--- app/models.py
+++ app/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+import os
+from deepgram import settings
 
-# Create your models here.
+
+def path_file():
+    def wrapper(user, filename):
+        file_upload_dir = os.path.join(settings.MEDIA_ROOT, 'audio')
+        if os.path.exists(file_upload_dir):
+            import shutil
+            shutil.rmtree(file_upload_dir)
+        return os.path.join(file_upload_dir, filename)
+    return wrapper
 
 
 class Audio_store(models.Model):
-    record = models.FileField(upload_to='audio/')
+    record = models.FileField(upload_to=path_file())
 
     class Meta:
         db_table = 'Audio_store'
