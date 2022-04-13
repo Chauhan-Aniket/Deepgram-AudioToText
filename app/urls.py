1c4edd7af209078984861fa0eee1aad7fa2b996c
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-13 10:31:01 +0530

    rename audio url path

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import HomePageView, Audio_store
+from .views import HomePageView, Audio_upload
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("audio/", Audio_store)
+    path("form/", Audio_upload, name="audio_upload"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
