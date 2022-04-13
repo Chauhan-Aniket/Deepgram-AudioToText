044e8c5ca2f0e2f588707397ae68a3ab8b148147
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-13 11:32:11 +0530

    get media file path

--- app/views.py
+++ app/views.py
@@ -3,18 +3,25 @@ from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import AudioForm
+from django.core.files.storage import FileSystemStorage
 
 
 class HomePageView(TemplateView):
     template_name = "home.html"
 
 
-def Audio_store(request):
+def Audio_upload(request):
     if request.method == 'POST':
         form = AudioForm(request.POST, request.FILES or None)
         if form.is_valid():
             form.save()
-            return render(request, 'home.html')
+            myfile = request.FILES['record']
+            fs = FileSystemStorage()
+            uploaded_file_url = fs.url(myfile)
+            print(uploaded_file_url)
+            return render(request, 'home.html', {
+                'uploaded_file_url': uploaded_file_url
+            })
     else:
         form = AudioForm()
-    return render(request, 'audio.html', {'form': form})
+    return render(request, 'form.html', {'form': form})
