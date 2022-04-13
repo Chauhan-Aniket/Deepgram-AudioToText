4b1b97a4c9a13f67674d73625f55e0c164ec6fb1
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-13 15:37:55 +0530

    get transcript via deepgram-sdk

--- app/views.py
+++ app/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import AudioForm
 from django.core.files.storage import FileSystemStorage
+from deepgram_app.settings import *
+# deepgram sdk
+from deepgram import Deepgram
+import asyncio
+import json
 
 
 class HomePageView(TemplateView):
@@ -19,9 +24,25 @@ def Audio_upload(request):
             fs = FileSystemStorage()
             uploaded_file_url = fs.url(myfile)
             print(uploaded_file_url)
+            asyncio.run(main(uploaded_file_url))
             return render(request, 'audio.html', {
                 'uploaded_file_url': uploaded_file_url
             })
     else:
         form = AudioForm()
     return render(request, 'form.html', {'form': form})
+
+
+DEEPGRAM_API_KEY = env('DEEPGRAM_API')
+
+
+async def main(PATH_TO_FILE):
+    # init the Deepgram SDK
+    deepgram = Deepgram(DEEPGRAM_API_KEY)
+    # open audio file
+    audio = open('.{}'.format(PATH_TO_FILE), 'rb')
+    source = {'buffer': audio, 'mimetype': 'audio/mpeg'}
+    response = await deepgram.transcription.prerecorded(source, {'punctuate': True})
+    # save it to json
+    audio_json = open('./audio.json', 'w')
+    audio_json.write(json.dumps(response, indent=2))
