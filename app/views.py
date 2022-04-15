e338fd417724c24772172bc4c862204be02a2359
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-15 13:20:26 +0530

    modified upload function

--- app/views.py
+++ app/views.py
@@ -15,22 +15,23 @@ class HomePageView(TemplateView):
     template_name = "home.html"
 
 
-def Audio_upload(request):
+def audio_upload(request):
+    uploaded_file = {}
     if request.method == 'POST':
         form = AudioForm(request.POST, request.FILES or None)
         if form.is_valid():
             form.save()
             myfile = request.FILES['record']
             fs = FileSystemStorage()
-            uploaded_file_url = fs.url(myfile).replace(
+            uploaded_file['url'] = fs.url(myfile).replace(
                 '%', '_').replace('20', '').replace(' ', '_')
-            print(uploaded_file_url)
-            asyncio.run(main(uploaded_file_url))
+            print(uploaded_file)
+            asyncio.run(main(uploaded_file['url']))
             with open('./audio.json', 'r') as j:
                 contents = json.loads(j.read())
-            return render(request, 'audio.html', {
-                'uploaded_file_url': uploaded_file_url, 'audio_content': contents['results']['channels'][0]
-                ['alternatives'][0]['transcript']})
+        return render(request, 'audio.html', {
+            'uploaded_file': uploaded_file, 'audio_contents': contents['results']['channels'][0]
+            ['alternatives'][0]['transcript']})
     else:
         form = AudioForm()
     return render(request, 'form.html', {'form': form})
