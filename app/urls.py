d2a3b954cd9b046785958c24766bdb0e5fb42cfa
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-15 13:21:03 +0530

    modified function name

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import HomePageView, Audio_upload
+from .views import HomePageView, audio_upload
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("form/", Audio_upload, name="audio_upload"),
+    path("form/", audio_upload, name="audio_upload"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
