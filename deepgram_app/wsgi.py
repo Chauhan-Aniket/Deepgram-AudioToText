5e6a32a05ce828e52c2d15ddc96aa4d72be4c2c2
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-15 17:40:51 +0530

    modified os environ

--- deepgram_app/wsgi.py
+++ deepgram_app/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deepgram_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'deepgram_app.settings.dev')
 
 application = get_wsgi_application()
