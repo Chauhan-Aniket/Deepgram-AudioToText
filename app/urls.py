8e68783f00c2fe0f26c2165bbaad95c609bfa698
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-16 13:10:01 +0530

    added admin url

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.contrib import admin
 from .views import HomePageView, audio_upload
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", HomePageView.as_view(), name="home"),
     path("form/", audio_upload, name="audio_upload"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
