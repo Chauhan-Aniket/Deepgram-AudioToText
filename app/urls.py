5d78ce1ef82a878a649fbd6f29d788a987a99856
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-16 14:28:04 +0530

    added favicon path

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import HomePageView, audio_upload
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("form/", audio_upload, name="audio_upload"),
+    path('favicon.png', RedirectView.as_view(
+        url='favicon.png'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
