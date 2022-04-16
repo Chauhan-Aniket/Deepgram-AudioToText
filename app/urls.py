170b05dfe445b0cb9612cc6a3b5497beb10a8bef
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-16 14:52:12 +0530

    modified extension png to ico

--- app/urls.py
+++ app/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import HomePageView, audio_upload
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("form/", audio_upload, name="audio_upload"),
-    path('favicon.png', RedirectView.as_view(
-        url='favicon.png'))
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('favicon.ico')))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
