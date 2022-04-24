e3381e76b3e7996641aee1e83d1340c551813360
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-24 21:10:01 +0530

    modified database to postgresql

--- deepgram_app/settings.py
+++ deepgram_app/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 import environ
 import django_heroku
+import dj_database_url
 
 env = environ.Env()
 # reading .env file
@@ -83,10 +84,9 @@ WSGI_APPLICATION = 'deepgram_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=env('DATABASE_URL')
+    )
 }
 
 
