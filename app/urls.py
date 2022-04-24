c093635df235b1d377fc03659ca9dfd8c9e0288d
Chauhan-Aniket <aniketchauhan2125@gmail.com>
2022-04-24 19:08:24 +0530

    modify home link

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import HomePageView, audio_upload
+from .views import audio_upload
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name="home"),
-    path("form/", audio_upload, name="audio_upload"),
+    path("", audio_upload, name="home"),
     path(
         "favicon.ico",
         RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
